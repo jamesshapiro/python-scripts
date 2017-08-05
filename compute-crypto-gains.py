c39cb7ed5bb7f3e9ccb5443901daeda60afbe41a
James Shapiro <jamesshapiro@gmail.com>
2017-08-04 20:13:22 -0600

    minor idiomatic changes to crypto profit calculator

--- compute-crypto-gains.py
+++ compute-crypto-gains.py
@@ -10,15 +10,14 @@ def extract_price(currency_symbol):
     url = "https://min-api.cryptocompare.com/data/price?fsym={}&tsyms=USD".format(currency_symbol)
     request = requests.get(url).text
     price_map = json.loads(request)
-    price = float(price_map["USD"])
+    price = price_map["USD"]
     return price
 
 def compute_gains(portfolio):
     quantities = portfolio.values()
-    prices = list(map(extract_price, portfolio.keys()))
-    quantities_and_prices = zip(quantities, prices)
+    prices = map(extract_price, portfolio.keys())
     
-    portfolio_value = sum([quantity * price for (quantity, price) in quantities_and_prices])
+    portfolio_value = sum([quantity * price for (quantity, price) in zip(quantities, prices)])
     return portfolio_value - initial_investment
 
 gains = compute_gains(portfolio)
