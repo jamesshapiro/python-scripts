b4c9d95e56e354a3382103e14400d2d30cffa14c
James Shapiro <jamesshapiro@gmail.com>
2018-01-14 12:57:09 -0700

    rewrite secure password gen to use docopt for arg handling and help message generation

--- password-generators/secure-password-generator.py
+++ password-generators/secure-password-generator.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3 -tt
-"""Example of program which uses [options] shortcut in pattern.
 
-Usage:
-  options_shortcut_example.py [options]
+"""Usage:
+  secure-password-generator.py [options]
 
 Options:
   -h --help                show this help message and exit
@@ -16,10 +15,59 @@ Options:
   --length LENGTH          Password length [default: 16]
   --numpasswords NUM       Number of passwords [default: 1]
   --annoyingreqs           1+ uppercase, lowercase, number and symbol
-  --justpasswords          Do not display any prompts
 """
+import random
 from docopt import docopt
 
-if __name__ == '__main__':
-    arguments = docopt(__doc__, version='2.0')
-    print(arguments)
+arguments = docopt(__doc__, version='2.0')
+    
+lower = 'abcdefghijklmnopqrstuvwxyz'
+upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+number = '0123456789'
+loIO01 = 'loIO01'
+symbol = '!@#$%^&*_+?-='
+symbolAmbiguous = '{}[]()/\\~,;:.<>'
+
+def exclude(characters, alphabet):
+    return "".join(list(set(alphabet) - set(characters)))
+
+def get_alphabet(arguments):
+    alphabet = ''.join([lower, upper, number, symbol, symbolAmbiguous])
+    if not arguments['--yesalphaambi']:
+        alphabet = exclude(loIO01, alphabet)
+    if arguments['--nolower']:
+        alphabet = exclude(lower, alphabet)
+    if arguments['--noupper']:
+        alphabet = exclude(upper, alphabet)
+    if arguments['--nonumber']:
+        alphabet = exclude(number, alphabet)
+    if arguments['--nosymbolambi'] and symbolAmbiguous in args:
+        alphabet = exclude(symbolAmbiguous, alphabet)
+    if arguments['--nosymbol']:
+        alphabet = exclude(symbolAmbiguous, alphabet)
+        alphabet = exclude(symbol, alphabet)
+    return alphabet
+
+def generate_passwords(arguments):
+    alphabet = get_alphabet(arguments)
+    num_passwords = int(arguments['--numpasswords'])
+    for _ in range(num_passwords):
+        length = int(arguments['--length'])
+        password = "".join([random.choice(alphabet) for _ in range(length)])
+        if arguments['--annoyingreqs']:
+            lower_set = ''.join(list(set(lower) - set(loIO01)))
+            upper_set = ''.join(list(set(upper) - set(loIO01)))
+            number_set = ''.join(list(set(number) - set(loIO01)))
+            special_char_sets = [lower_set, upper_set, number_set, symbol]
+            password = password[4:]
+            password += ''.join([random.choice(char_set) for char_set in special_char_sets])
+            password_list = list(password)
+            random.shuffle(password_list)
+            password = ''.join(password_list)
+        print(password)
+
+def __main__():
+    args = [lower, upper, number, symbol, symbolAmbiguous]
+    generate_passwords(arguments)
+
+__main__()
