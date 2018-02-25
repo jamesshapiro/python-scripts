2bf2d5dd1bfa87acbf929bafd0444abe45fd393b
James Shapiro <jamesshapiro@gmail.com>
2018-02-24 18:56:14 -0800

    add weekday support

--- lambda/remindme.py
+++ lambda/remindme.py
@@ -9,9 +9,9 @@ Options:
   --until STOP        repeat until, e.g. 5-14-2020-9:15am [default: '1-1-2038-12:00am']
 """
 
-#now.datetime(to_timezone='US/Pacific', naive=True)
-#print('Usage: remindme "do laundry" 12-18-2016-12:09:am')
+#print('Usage: remindme "do laundry" 12-18-2016-12:09am')
 
+import datetime
 import json
 import maya
 import re
@@ -22,6 +22,14 @@ from docopt import docopt
 """
 today, tomorrow, M-Sun, next week, tonight
 """
+monday = ['monday','mon','m']
+tuesday = ['tuesday','tue','tues','t', 'tu']
+wednesday = ['wednesday' ,'wed','weds','w']
+thursday = ['thursday','thu','thurs','thur','th']
+friday = ['friday','fri', 'f']
+saturday = ['saturday', 'sat', 'sa']
+sunday = ['sunday', 'sun', 'su']
+weekdays = monday + tuesday + wednesday + thursday + friday + saturday + sunday
 
 def time_of_day(time):
     # 8am, 8pm, 12am, 8:30am, 8:30pm, 12:30pm
@@ -71,7 +79,34 @@ def upload_reminder(time, reminder, password, url):
     to_json = json.loads(r.text)
     print(to_json['message'])
 
-#TODO: add Sunday, Mon-Fri, Sat, next week, next month    
+def get_weekday_index(weekday):
+    if weekday in monday:
+        return 0
+    elif weekday in tuesday:
+        return 1
+    elif weekday in wednesday:
+        return 2
+    elif weekday in thursday:
+        return 3
+    elif weekday in friday:
+        return 4
+    elif weekday in saturday:
+        return 5
+    elif weekday in sunday:
+        return 6
+    else:
+        print('expected a weekday or weekday abbreviation but got: {}'.format(weekday))
+        sys.exit(0)
+
+def get_days_from_today(target_weekday):
+    # datetime weekday is a value from 0 to 6: Monday is 0, Thursday is 3, Sunday is 6
+    today_weekday = datetime.datetime.today().weekday()
+    target_weekday = get_weekday_index(target_weekday)
+    if today_weekday < target_weekday:
+        return target_weekday - today_weekday
+    else:
+        return 7 - today_weekday + target_weekday
+
 def parse_time(time, midnight):
     if time[0].endswith('am') or time[0].endswith('pm'):
         return james_date_to_epoch(time[0])
@@ -85,11 +120,14 @@ def parse_time(time, midnight):
         return midnight.add(hours=hours, minutes=minutes)
     if time[0] == 'tomorrow':
         return midnight.add(days=1, hours=hours, minutes=minutes)
+    if time[0].lower() in weekdays:
+        days = get_days_from_today(time[0])
+        return midnight.add(days=days, hours=hours, minutes=minutes)
     return midnight
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Reminders 0.1')
-    print(arguments)
+    #print(arguments)
     with open('/Users/jamesshapiro/scripts/lambda_creds', 'r') as json_data:
         d = json.load(json_data)
     password = d['password']
@@ -99,11 +137,12 @@ if __name__ == '__main__':
     midnight = get_midnight(timezone)
     time = parse_time(arguments['TIME'], midnight)
     time = str(int(time._epoch))
+    print(time)
     #TODO: finish implementing repeating reminders
     upload_reminder(time, reminder, password, url)
     if arguments['--repeat']:
         stop_date = james_date_to_epoch(arguments['--until'])
-        print(int(stop_date._epoch))
+        #print(int(stop_date._epoch))
 
 """
 now = maya.now()
