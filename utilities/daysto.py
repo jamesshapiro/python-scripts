c7f2b78078731dc828be939311266eb49639e090
James Shapiro <jamesshapiro@gmail.com>
2018-04-04 22:26:43 -0700

    drastically simplfiy daysto

--- utilities/daysto.py
+++ utilities/daysto.py
@@ -7,48 +7,14 @@ Options:
   -h --help           show this HELP message and exit
 """
 
-import datetime
-import json
-import maya
-import re
-import sys
+from datetime import date
 from docopt import docopt
-
-def james_date_to_epoch(james_date):
-    parts = james_date.split('-')
-    if len(parts) != 4:
-        print('Usage: daysto 01-01-2100 (also works with past dates)')
-        raise ValueError("Bad Time Syntax!")
-    month = parts[0]
-    day = parts[1]
-    year = parts[2]
-    maya_format = '{}-{}-{} {}:{}'.format(year, month, day, 0, 0)
-    return maya.when(maya_format,timezone=timezone)
-
-def get_midnight(timezone):
-    now = maya.now()
-    local_now = now.datetime(to_timezone=timezone, naive=True)
-    year = local_now.year
-    month = local_now.month
-    day = local_now.day
-    midnight = maya.when('-'.join(map(str,[year, month, day])), timezone=timezone)
-    return midnight
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Reminders 0.1')
-    with open('/Users/jamesshapiro/scripts/lambda_creds', 'r') as json_data:
-        d = json.load(json_data)
-    timezone = d['timezone']
-    target_day = arguments['DATE']
-    midnight = get_midnight(timezone)
-    target_midnight = james_date_to_epoch(target_day + '-12am')
-#    print(midnight, target_midnight)
-    total = 0
-    while midnight._epoch < target_midnight._epoch:
-        midnight = midnight.add(days=1)
-        total += 1
-    while midnight._epoch > target_midnight._epoch:
-        midnight = midnight.add(days=-1)
-        total -= 1
-    print(total)
-
+    today = date.today()
+    target_date = arguments['DATE']
+    month, day, year = map(int, target_date.split('-'))
+    target_date = date(year, month, day)
+    delta = str(target_date - today).split(' ')
+    print(delta[0])
